75f71b9d613180e4c7f305e14757fe7cad72a091
z4nky074 <rafa24052005@gmail.com>
2024-11-23 10:30:17 +0700

    Update CVDownload.js

--- src/components/CVDownload.js
+++ src/components/CVDownload.js
@@ -4,11 +4,8 @@ const CVDownload = () => {
   return (
     <div className="text-center">
       <h2>Download My CV</h2>
-      {/* Link untuk membuka atau mendownload CV */}
-      <a href="/cv-ats.pdf" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
-        Lihat CV
-      </a>
-      <a href="/cv-ats.pdf" download className="btn btn-secondary mt-2">
+      {/* Link untuk mendownload CV */}
+      <a href="/cv-ats.pdf" download className="btn btn-primary mt-2">
         Download CV
       </a>
     </div>
